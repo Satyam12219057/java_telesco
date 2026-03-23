237a2729208ad13403d47528c50adb8423fa89b3
satyam kumar <satyamlkr2004@gmail.com>
2026-03-23 16:37:42 +0530

    //List with the user define class and functions

--- Advance1.java
+++ Advance1.java
@@ -29,28 +29,84 @@
 
 
 //here we are going to store the value with the proper index
+// import java.util.Collection;
+// import java.util.HashMap;
+// import java.util.Map;
+
+// public class Advance1{
+//     public static void main(String[]args){
+//         Map<String, Integer>student=new HashMap<>();
+//         student.put("navin",10);
+//         student.put("Satyam",100);
+//         student.put("na",120);
+//         student.put("Sa",130);
+//         System.out.println(student);
+
+//         System.out.println(student.get("navin"));
+        
+//         //we have to get all the keys
+//         for(String key:student.keySet()){
+//             System.out.println(key +" "+student.get(key));
+//         }
+
+        
+
+
+
+//     }
+// }
+
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+//now we are lern the use of the list with the user defined dataTYpe
+class Student{
+    public int age;
+    public String name;
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Student [age=" + age + ", name=" + name + "]";
+    }
+
+    
+}
+
 
 public class Advance1{
     public static void main(String[]args){
-        Map<String, Integer>student=new HashMap<>();
-        student.put("navin",10);
-        student.put("Satyam",100);
-        student.put("na",120);
-        student.put("Sa",130);
-        System.out.println(student);
+        Comparator<Integer>com=new Comparator<Integer>() {
+            public int compare(Integer i,Integer j){
+                if(i%10> j%10){
+                    return 1;
+                }
+                else{
+                    return 0;
+                }
+            }
+        };
+       
+        List<Integer>nums=new ArrayList<>();
+        nums.add(4);
+        nums.add(29);
+        nums.add(6);
+        nums.add(7);
+        System.out.println(nums);
+       Collections.sort(nums);
+       System.out.println(nums);
 
-        System.out.println(student.get("navin"));
-        
-        //we have to get all the keys
-        for(String key:student.keySet()){
-            System.out.println(key +" "+student.get(key));
-        }
-
-        
-
+       //
+       List<Student>nums1=new ArrayList<Student>();
+       nums1.add(new Student(20,"staym"));
+       nums1.add(new Student(21,"Ritvik"));
+       nums1.add(new Student(22,"Aman"));
+       System.out.println(nums1);
 
 
     }
